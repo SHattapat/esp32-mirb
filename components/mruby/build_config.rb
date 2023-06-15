9d03244c2a46880f97155a780886e503bb496805
SHattapat <hattaphat22642@gmail.com>
2023-06-15 16:07:11 +0900

    Edit lib

--- components/mruby/build_config.rb
+++ components/mruby/build_config.rb
@@ -91,7 +91,7 @@ MRuby::CrossBuild.new('esp32') do |conf|
   conf.gem :git => 'https://github.com/matsumoto-r/mruby-sleep.git' 
   conf.gem '/home/wave/esp/work/mruby-yourname' 
   #conf.gem '/home/wave/esp/gems' 
-  conf.gem '/home/wave/esp/my_mruby_project/components/mruby/mruby/build/repos/esp32/mruby-standard-gpio'
+  conf.gem '/home/wave/esp/my_mruby_project/components/mruby/mruby/build/repos/mruby-standard-gpio'
   #conf.gem '/home/wave/esp/Test-mrbgems/mruby-gpio'
   
 
